6e7298c71ec93c6b22ede9205f0d1af025ed3ff5
Aiman Arshiya <aimanarshiyaazeem@gmail.com>
2026-07-31 12:33:28 +0530

    unnoticed

--- navigation/BottomTabs.js
+++ navigation/BottomTabs.js
@@ -3,7 +3,11 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 const Tab = createBottomTabNavigator();
 export default function BottomTabs(){
     return(
-        
+        <View>
+            <Text>
+                BottomTab
+            </Text>
+        </View>
 
     );
 }
